4db7e44a205de964d65922a00b88c9eae10633fa
Farruco Sanjurjo <farruco.sanjurjo@trabesoluciones.net>
2012-03-22 19:01:20 +0100

    Define test for wsecli_key

--- test/spec/wsecli_key_spec.erl
+++ test/spec/wsecli_key_spec.erl
@@ -1,7 +1,18 @@
 -module(wsecli_key_spec).
 -include_lib("espec/include/espec.hrl").
+-include_lib("hamcrest/include/hamcrest.hrl").
 
 spec() ->
   describe("wsecli_key", fun() ->
-        it("should return a valid Sec-WebSocket-Key")
+        it("should return a valid Sec-WebSocket-Key", fun() ->
+              %Meck crashes if we try to mock crypto module
+              %meck:new(crypto, [passthrough]),
+              meck:new(base64, [unstick, passthrough]),
+
+              <<_Key:16>> = wsecli_key:generate(),
+
+              %assert_that(meck:called(crypto, rand_bytes, 16), is(true)),
+              assert_that(meck:called(base64, encode, '_'), is(true)),
+              meck:unload(base64)
+          end)
     end).
